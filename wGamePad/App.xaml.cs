351fe1d7979162f917d7480204badf6a2b3cbe47
realpotsystems <manataku@me.com>
2014-08-16 10:04:24 +0900

    多重起動抑止処理を追加

--- wGamePad/App.xaml.cs
+++ wGamePad/App.xaml.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿//------------------------------------------------------------
+//
+//  仮想ゲームパッド vGamePad
+//  © 2014,RealPotSystems(TAKUBON). All rights reserverd.
+//
+//  Workfile : App.xaml.cs
+//  Author   : TAKUYA MANABE(manataku@me.com)
+//
+//------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace vGamePad
@@ -13,5 +23,45 @@ namespace vGamePad
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 多重起動抑止のミューテックス
+        /// </summary>
+        private Mutex mutex = new Mutex(false, "vGamePad");
+        /// <summary>
+        /// vGamePadのメインウィンドウ
+        /// </summary>
+        private MainWindow main = null;
+        /// <summary>
+        /// vGamePadのスタートアップ処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (!mutex.WaitOne(0, false))
+            {
+                mutex.Close();
+                mutex = null;
+                Shutdown(-1);
+            }
+            else
+            {
+                main = new MainWindow();
+                main.Show();
+            }
+        }
+        /// <summary>
+        /// vGamePadの終了処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            if (mutex != null)
+            {
+                mutex.ReleaseMutex();
+                mutex.Close();
+            }
+        }
     }
 }
