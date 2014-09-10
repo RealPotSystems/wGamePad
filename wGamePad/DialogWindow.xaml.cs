691969a18e1f6658002120fa10f5e026e5b5f134
realpotsystems <manataku@me.com>
2014-09-10 20:47:56 +0900

    レイアウトの選択機能を実�

--- wGamePad/DialogWindow.xaml.cs
+++ wGamePad/DialogWindow.xaml.cs
@@ -19,13 +19,18 @@ namespace vGamePad.DialogWindow
     /// </summary>
     public partial class DialogWindow : Window
     {
+        public int? result { private set; get; }
+
         const string _OK = "\uE17E\uE171";          // !
         const string _OKCANCEL = "\uE17E\uE11B";    // ?
+
         public enum DialogStyle
         {
             OK,
-            OKCANCEL
+            OKCANCEL,
+            ORIGINAL
         }
+
         public DialogWindow(string t, string m, DialogStyle s = DialogStyle.OK)
         {
             InitializeComponent();
@@ -38,11 +43,18 @@ namespace vGamePad.DialogWindow
                 Type.Content = _OK;
                 OK.Visibility = System.Windows.Visibility.Visible;
             }
-            else
+            else if (s == DialogStyle.OKCANCEL)
             {
                 Type.Content = _OKCANCEL;
                 Yes.Visibility = System.Windows.Visibility.Visible;
                 No.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                Type.Content = _OKCANCEL;
+                Botton1.Visibility = System.Windows.Visibility.Visible;
+                Botton2.Visibility = System.Windows.Visibility.Visible;
+                Botton3.Visibility = System.Windows.Visibility.Visible;
             }
         }
 
@@ -57,6 +69,25 @@ namespace vGamePad.DialogWindow
         {
             PlayButtonSound.Play();
             DialogResult = false;
+            Close();
+        }
+
+        private void OnBottonClick(object sender, RoutedEventArgs e)
+        {
+            PlayButtonSound.Play();
+            var botton = (Button)sender;
+            switch(botton.Name)
+            {
+                case "Botton1":
+                    result = 1;
+                    break;
+                case "Botton2":
+                    result = 2;
+                    break;
+                case "Botton3":
+                    result = 3;
+                    break;
+            }
             Close();
         }
     }
