56b9872b82d83e057617005a6936965ca65f9339
realpotsystems <manataku@me.com>
2014-09-11 01:49:38 +0900

    レイアウトの反映機能追加

--- wGamePad/LayoutSetting.xaml.cs
+++ wGamePad/LayoutSetting.xaml.cs
@@ -46,5 +46,32 @@ namespace vGamePad
             Layout2.Content = string.Format("{0} {1}", n == 2 ? check_on : check_off, "レイアウト2");
             Layout3.Content = string.Format("{0} {1}", n == 0 ? check_on : check_off, "デフォルト レイアウト");
         }
+
+        private void LayoutClick(object sender, RoutedEventArgs e)
+        {
+            PlayButtonSound.Play();
+            switch(((Button)sender).Name)
+            {
+                case "Layout1":
+                    Properties.Settings.Default.Layout = 1;
+                    break;
+                case "Layout2":
+                    Properties.Settings.Default.Layout = 2;
+                    break;
+                case "Layout3":
+                    Properties.Settings.Default.Layout = 0;
+                    break;
+            }
+            int n = Properties.Settings.Default.Layout;
+
+            Layout1.Content = string.Format("{0} {1}", n == 1 ? check_on : check_off, "レイアウト1");
+            Layout2.Content = string.Format("{0} {1}", n == 2 ? check_on : check_off, "レイアウト2");
+            Layout3.Content = string.Format("{0} {1}", n == 0 ? check_on : check_off, "デフォルト レイアウト");
+
+            // 選択されたレイアウトを適用する
+            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.SetLayout();
+            mainWindow.SetConfig();
+        }
     }
 }
