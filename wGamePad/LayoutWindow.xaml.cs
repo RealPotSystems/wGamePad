32302df45c39ce5bc10097630a3b5c48ae4bdd61
realpotsystems <manataku@me.com>
2014-09-23 16:31:10 +0900

    version 0.9.3.1

--- wGamePad/LayoutWindow.xaml.cs
+++ wGamePad/LayoutWindow.xaml.cs
@@ -560,6 +560,8 @@ namespace vGamePad
             {
                 layoutDic = vLayoutControl.LoadLayout(9);
                 SetButtonLayout();
+                colorChanged.Foreground = Properties.Settings.Default.Foreground;
+                colorChanged.Background = Properties.Settings.Default.Background;
             }
         }
 
